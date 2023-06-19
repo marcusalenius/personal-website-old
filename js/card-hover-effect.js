b0f7789ac90c2f991ba5b1fd63fb8821f4ed15cf
Marcus Alenius <marcus.alenius@gmail.com>
2023-06-19 00:17:25 -0400

    added hover effect to hero-card

--- js/card-hover-effect.js
+++ js/card-hover-effect.js
@@ -1,11 +1,22 @@
+// Apply hover effect to cards in card layout
 for (const cardLayout of document.querySelectorAll('.card-layout')) {
     cardLayout.onmousemove = event => {
         for (const card of document.querySelectorAll('.card')) {
             const rect = card.getBoundingClientRect();
             const x = event.clientX - rect.left;
             const y = event.clientY - rect.top;
-            card.style.setProperty('--mouse-x', `${x}px`)
-            card.style.setProperty('--mouse-y', `${y}px`)
+            card.style.setProperty('--mouseX', `${x}px`)
+            card.style.setProperty('--mouseY', `${y}px`)
         }
     }
+}
+
+// Apply hover effect to hero-card
+const heroCard = document.getElementById('hero-card')
+heroCard.onmousemove = event => {
+    const rect = heroCard.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    heroCard.style.setProperty('--mouseX', `${x}px`)
+    heroCard.style.setProperty('--mouseY', `${y}px`)
 }
