b1d92630cbdd7cd4f310ccae01f9f73c8fd5737d
Marcus Alenius <marcus.alenius@gmail.com>
2023-06-19 20:08:43 -0400

    added hover effect to appearance button in kenboard post

--- js/card-hover-effect.js
+++ js/card-hover-effect.js
@@ -1,66 +1,93 @@
 // Apply hover effect to cards in card layout
-for (const cardLayout of document.querySelectorAll('.card-layout')) {
-    cardLayout.onmousemove = event => {
-        for (const card of document.querySelectorAll('.card')) {
-            const rect = card.getBoundingClientRect();
-            const x = event.clientX - rect.left;
-            const y = event.clientY - rect.top;
-            card.style.setProperty('--mouseX', `${x}px`)
-            card.style.setProperty('--mouseY', `${y}px`)
+const cardLayouts = document.querySelectorAll('.card-layout');
+if (cardLayouts !== null) {
+    for (const cardLayout of cardLayouts) {
+        cardLayout.onmousemove = event => {
+            for (const card of document.querySelectorAll('.card')) {
+                const rect = card.getBoundingClientRect();
+                const x = event.clientX - rect.left;
+                const y = event.clientY - rect.top;
+                card.style.setProperty('--mouseX', `${x}px`);
+                card.style.setProperty('--mouseY', `${y}px`);
+            }
         }
-    }
+    }    
 }
 
 // Apply hover effect to hero-card
-const heroCard = document.getElementById('hero-card')
-heroCard.onmousemove = event => {
-    const rect = heroCard.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    heroCard.style.setProperty('--mouseX', `${x}px`)
-    heroCard.style.setProperty('--mouseY', `${y}px`)
+const heroCard = document.getElementById('hero-card');
+if (heroCard !== null) {
+    heroCard.onmousemove = event => {
+        const rect = heroCard.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        heroCard.style.setProperty('--mouseX', `${x}px`);
+        heroCard.style.setProperty('--mouseY', `${y}px`);
+    }    
 }
 
 // Apply hover effect to contact cards
-for (const cardLayout of document.querySelectorAll('.contact-card-layout')) {
-    cardLayout.onmousemove = event => {
-        for (const card of document.querySelectorAll('.contact-card')) {
-            const rect = card.getBoundingClientRect();
-            const x = event.clientX - rect.left;
-            const y = event.clientY - rect.top;
-            card.style.setProperty('--mouseX', `${x}px`)
-            card.style.setProperty('--mouseY', `${y}px`)
+const contactCardLayouts = document.querySelectorAll('.contact-card-layout');
+if (contactCardLayouts !== null) {
+    for (const contactCardLayout of contactCardLayouts) {
+        contactCardLayout.onmousemove = event => {
+            for (const card of document.querySelectorAll('.contact-card')) {
+                const rect = card.getBoundingClientRect();
+                const x = event.clientX - rect.left;
+                const y = event.clientY - rect.top;
+                card.style.setProperty('--mouseX', `${x}px`);
+                card.style.setProperty('--mouseY', `${y}px`);
+            }
         }
-    }
+    }    
 }
 
 // Apply hover effect to nav-right navbuttons
-document.getElementById('nav-right').onmousemove = event => {
-    for (const navbutton of document.querySelectorAll('.navbutton')) {
-        const rect = navbutton.getBoundingClientRect();
-        const x = event.clientX - rect.left;
-        const y = event.clientY - rect.top;
-        navbutton.style.setProperty('--mouseX', `${x}px`)
-        navbutton.style.setProperty('--mouseY', `${y}px`)
-    }
+const navRight = document.getElementById('nav-right');
+if (navRight !== null) {
+    navRight.onmousemove = event => {
+        for (const navbutton of document.querySelectorAll('.navbutton')) {
+            const rect = navbutton.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+            navbutton.style.setProperty('--mouseX', `${x}px`);
+            navbutton.style.setProperty('--mouseY', `${y}px`);
+        }
+    }    
 }
 
 // Apply hover effect to appearance drop-down
-const appearanceDropDown = document.getElementById('appearance-drop-down')
-appearanceDropDown.onmousemove = event => {
-    const rect = appearanceDropDown.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    appearanceDropDown.style.setProperty('--mouseX', `${x}px`)
-    appearanceDropDown.style.setProperty('--mouseY', `${y}px`)
+const appearanceDropDown = document.getElementById('appearance-drop-down');
+if (appearanceDropDown !== null) {
+    appearanceDropDown.onmousemove = event => {
+        const rect = appearanceDropDown.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        appearanceDropDown.style.setProperty('--mouseX', `${x}px`);
+        appearanceDropDown.style.setProperty('--mouseY', `${y}px`);
+    }    
 }
 
-// Apply hover effects to navbutton-home
-const navbuttonHome = document.getElementById('navbutton-home')
-navbuttonHome.onmousemove = event => {
-    const rect = navbuttonHome.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    navbuttonHome.style.setProperty('--mouseX', `${x}px`)
-    navbuttonHome.style.setProperty('--mouseY', `${y}px`)
+// Apply hover effect to navbutton-home
+const navbuttonHome = document.getElementById('navbutton-home');
+if (navbuttonHome !== null) {
+    navbuttonHome.onmousemove = event => {
+        const rect = navbuttonHome.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        navbuttonHome.style.setProperty('--mouseX', `${x}px`);
+        navbuttonHome.style.setProperty('--mouseY', `${y}px`);
+    }    
+}
+
+// Apply hover effect to navbuttons in post pages
+const navbuttonAppearance = document.getElementById('navbutton-appearance');
+if (navbuttonAppearance !== null) {
+    navbuttonAppearance.onmousemove = event => {
+        const rect = navbuttonAppearance.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        navbuttonAppearance.style.setProperty('--mouseX', `${x}px`);
+        navbuttonAppearance.style.setProperty('--mouseY', `${y}px`);
+    }    
 }
